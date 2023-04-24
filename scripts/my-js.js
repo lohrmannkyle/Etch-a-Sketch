075dfab68c9401ca15c9a06a8a229bcabd9df538
kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-04-24 06:08:25 -0700

    Complete logic for board

--- scripts/my-js.js
+++ scripts/my-js.js
@@ -7,19 +7,42 @@ function addRow(){
 function addSquare(){
     const square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mouseover', () => {
+        const rand1 = Math.round(Math.random() * 255);
+        const rand2 = Math.round(Math.random() * 255);
+        const rand3 = Math.round(Math.random() * 255);
+        square.style.background = `rgb(${rand1},${rand2},${rand3})`;
+    })
     return square;
 }
 
-function loadGrid(){
-    const container = document.querySelector('body');
-    for (let i = 0; i < 16; i++){
+function loadGrid(size=16){
+    const container = document.querySelector('.grid-daddy');
+    for (let i = 0; i < size; i++){
         const row = addRow();
 
-        for (let j = 0; j < 16; j++){
+        for (let j = 0; j < size; j++){
             row.appendChild(addSquare());
         }
         container.appendChild(row);
     }
 }
 
+function removeGrid(){
+    console.log("removing grid...")
+    const all = document.querySelectorAll('.flex');
+    console.log(all.length);
+    all.forEach(div => div.remove());
+
+}
+
 loadGrid();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let newSize = Number(prompt("Grid Size: "));
+    console.log(typeof(newSize));
+    newSize = (newSize <= 100) ? newSize : 100;
+    removeGrid();
+    loadGrid(newSize);
+})
